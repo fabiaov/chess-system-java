abdbc2041eadcbdf6ab8f771221cf521e6c703e2
Fabio Vinicius <fabioviniciuslds@hotmail.com>
2021-05-20 22:06:43 -0300

    definindo algumas classes, camada Chess e imprimindo o tabuleiro

---
+++ src/Boardgame/Piece.java
@@ -0,0 +1,19 @@
+package Boardgame;
+
+public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
